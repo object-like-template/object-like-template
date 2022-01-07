77cab50453ae6118b065dadf90ad996861a610f1
Park Jeongseon <wjdtjs529@gmail.com>
2022-01-07 18:23:21 +0900

    Add: render test

--- spec/2-render.test.ts
+++ spec/2-render.test.ts
@@ -1,16 +1,133 @@
 import { expect } from 'chai';
-import path from 'path';
-import fs from 'fs';
-
 import render from '../lib/render';
 
-describe('partial template', () => {
-  it('should render a partial template', () => {
-    const template = '#{partial}';
-    const result = render(template);
-    const partialTemplate = fs.readFileSync(path.join(__dirname, 'partial.olt'), 'utf-8');
-    const renderedPartial = render(partialTemplate);
+describe('basic template', () => {
+  it('should render a basic template', () => {
+    const templatePath = '../views/basic.olt';
+    const result = render(templatePath);
 
-    expect(result).to.equal(renderedPartial);
+    expect(result).to.equal(
+      '<!DOCTYPE html>'
+        + '<html>'
+          + '<head>'
+            + '<title>basic</title>'
+          + '</head>'
+        + '<body>'
+          + '<section>'
+            + '<article>'
+              + '<h1>This is basic template</h1>'
+            + '</article>'
+          + '</section>'
+        + '</body>'
+      + '</html>',
+    );
+  });
+
+  it('should throw error when template path is wrong', () => {
+    const templatePath = '../views/notExist.olt';
+
+    expect(render(templatePath)).to.throw('invalid path');
+  });
+});
+
+describe('template including options', () => {
+  it('should render a template including options', () => {
+    const templatePath = '../views/includeOption.olt';
+    const result = render(templatePath, { title: 'This is basic template' });
+
+    expect(result).to.equal(
+      '<!DOCTYPE html>'
+        + '<html>'
+          + '<head>'
+            + '<title>basic</title>'
+          + '</head>'
+        + '<body>'
+          + '<section>'
+            + '<article>'
+              + '<h1>This is basic template</h1>'
+            + '</article>'
+          + '</section>'
+        + '</body>'
+      + '</html>',
+    );
+  });
+});
+
+describe('template including partial template', () => {
+  it('should render a template including partial template', () => {
+    const templatePath = '../views/includePartial';
+    const result = render(templatePath);
+
+    expect(result).to.equal(
+      '<!DOCTYPE html>'
+        + '<html>'
+          + '<head>'
+            + '<title>basic</title>'
+          + '</head>'
+        + '<body>'
+          + '<section>'
+            + '<article>'
+              + '<h1>This is basic template</h1>'
+            + '</article>'
+            + '<article>'
+              + '<h1>partial</h1>'
+              + '<p>This is an article about partial.</p>'
+            + '</article>'
+          + '</section>'
+        + '</body>'
+      + '</html>',
+    );
+  });
+
+  it('should render a template including partial with option', () => {
+    const templatePath = '../views/includePartial';
+    const result = render(templatePath);
+
+    expect(result).to.equal(
+      '<!DOCTYPE html>'
+        + '<html>'
+          + '<head>'
+            + '<title>basic</title>'
+          + '</head>'
+        + '<body>'
+          + '<section>'
+            + '<article>'
+              + '<h1>This is basic template</h1>'
+            + '</article>'
+            + '<article>'
+              + '<h1>partial</h1>'
+              + '<p>template</p>'
+            + '</article>'
+          + '</section>'
+        + '</body>'
+      + '</html>',
+    );
+  });
+});
+
+describe('template including options and partial template', () => {
+  it('should render a template including nested options', () => {
+    const templatePath = '../views/includeOption.olt';
+    const result = render(templatePath, { article: { title: 'partial', content: 'This is an article about partial.' } });
+
+    expect(result).to.equal(
+      '<!DOCTYPE html>'
+        + '<html>'
+          + '<head>'
+            + '<title>basic</title>'
+          + '</head>'
+        + '<body>'
+          + '<section>'
+            + '<article>'
+              + '<h1>This is basic template</h1>'
+            + '</article>'
+            + '<article>'
+              + '<h1>partial</h1>'
+              + '<p>This is an article about partial.</p>'
+            + '</article>'
+          + '</section>'
+        + '</body>'
+      + '</html>',
+    );
   });
 });
