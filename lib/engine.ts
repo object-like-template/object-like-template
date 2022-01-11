df8fcc8d79965c6e5ce46e33d0d334283ca47641
ParkJeongseon <wjdtjs529@gmail.com>
2022-01-12 01:02:07 +0900

    Add: Pass express test

--- lib/engine.ts
+++ lib/engine.ts
@@ -1,5 +1,13 @@
+import { Options } from './convert';
+import render from './render';
+
 type Callback = (e: unknown, rendered?: string | undefined) => void
 
 export default function defineEngine(filePath: string, options: object, callback: Callback): void {
-  callback('error');
+  try {
+    const rendered = render(filePath, options as Options);
+    return callback(null, rendered);
+  } catch (err) {
+    return callback(err);
+  }
 }
